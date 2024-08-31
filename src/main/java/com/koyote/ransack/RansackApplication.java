565f431f28a5f8451e52e989c55f5fadefa6159b
Leonel Fonseca <leonelfonseca@gmail.com>
2024-08-31 15:06:37 +0100

    Name changing

---
+++ src/main/java/com/koyote/ransack/RansackApplication.java
@@ -0,0 +1,13 @@
+package com.koyote.ransack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RansackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RansackApplication.class, args);
+	}
+
+}
