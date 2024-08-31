c6949df44b40d947d0fe24abcfa16d37406fbdcc
Leonel Fonseca <leonelfonseca@gmail.com>
2024-08-31 15:06:37 +0100

    Name changing

---
+++ src/test/java/com/koyote/ransack/RansackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koyote.ransack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RansackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
