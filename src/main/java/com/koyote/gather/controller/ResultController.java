2b846bd12a034ae63bd95466781eca17f1faf854
Leonel Fonseca <leonelfonseca@gmail.com>
2024-09-04 19:33:43 +0100

    Enhance Controller

--- src/main/java/com/koyote/gather/controller/ResultController.java
+++ src/main/java/com/koyote/gather/controller/ResultController.java
@@ -1,19 +1,32 @@
 package com.koyote.gather.controller;
 
+import com.koyote.gather.service.GmailQuickstart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 
 @RestController
 public class ResultController {
-    @GetMapping("/results")
-    List<String> search(@RequestParam String search) {
 
-        ArrayList<String> list = new ArrayList<>();
-        list.add("You want to search for " + search);
-        return list;
+    private GmailQuickstart gmailQuickstart;
+    @Autowired
+    public ResultController(GmailQuickstart gmailQuickstart) {
+        this.gmailQuickstart = gmailQuickstart;
     }
+
+    @GetMapping("/results")
+    List<String> search(@RequestParam String search) throws GeneralSecurityException, IOException {
+
+
+        return gmailQuickstart.getMessages();
+
+    }
+
+
+
 }
